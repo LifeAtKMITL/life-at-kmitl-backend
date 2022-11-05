9737a1204f0ce1b8da4908b0fff5deca80efb609
Chawapon <chawapon2001@gmail.com>
2022-11-05 16:19:35 +0700

    fix findById User.  get All user sharenote

--- src/modules/sharenote/db/sharenote-dto.repository.ts
+++ src/modules/sharenote/db/sharenote-dto.repository.ts
@@ -1,34 +1,34 @@
 import { SharenoteSchema } from './sharenote-schema';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import {Model} from 'mongoose';
+import { Model } from 'mongoose';
 import { SharenotesDto } from '../dtos/sharenotes.dto';
 @Injectable()
-export class SharenoteDtoRepository{
-    constructor(
-        @InjectModel(SharenoteSchema.name)
-        private readonly sharenoteModel:Model<SharenoteSchema>,
-    ){}
-    async findAll(): Promise<SharenotesDto[]>{
-        const sharenotes = await this.sharenoteModel.find({}, {}, { lean: true });
-        return sharenotes.map((sharenote) => {
-            return {
-              userName: sharenote.userName,
-              userId: sharenote.userId,
-              sharenoteCollectionName : sharenote.sharenoteCollectionName,
-              sharenoteCollectionNameVersion: sharenote.sharenoteCollectionNameVersion,
-              files: sharenote.files,
-              likeCount : sharenote.likeCount,
-              dowloadCount: sharenote.dowloadCount,
-              teachers:sharenote.teachers,
-              date:sharenote.date,
-            };
-          });
-    }
+export class SharenoteDtoRepository {
+  constructor(
+    @InjectModel(SharenoteSchema.name)
+    private readonly sharenoteModel: Model<SharenoteSchema>,
+  ) {}
+  async findAll(): Promise<SharenotesDto[]> {
+    const sharenotes = await this.sharenoteModel.find({}, {}, { lean: true });
+    return sharenotes.map((sharenote) => {
+      return {
+        userName: sharenote.userName,
+        userId: sharenote.userId,
+        sharenoteCollectionName: sharenote.sharenoteCollectionName,
+        sharenoteCollectionNameVersion: sharenote.sharenoteCollectionNameVersion,
+        files: sharenote.files,
+        likeCount: sharenote.likeCount,
+        dowloadCount: sharenote.dowloadCount,
+        teachers: sharenote.teachers,
+        date: sharenote.date,
+      };
+    });
+  }
 
-    // DESC: Filter Subject Collection by subjectId
-  async findById(id: string): Promise<SharenotesDto> {
-    const subject = await this.sharenoteModel.findOne({ userId : id }, {}, { lean: true });
+  // DESC: Filter Subject Collection by subjectId
+  async findById(id: string): Promise<SharenotesDto[]> {
+    const subject = await this.sharenoteModel.find({ userId: id }, {}, { lean: true });
     return subject;
   }
 }
