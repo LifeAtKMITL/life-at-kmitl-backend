50828c32dadd684cfac11eef9d623adf03835e27
Chawapon <chawapon2001@gmail.com>
2022-11-12 21:42:42 +0700

    fix api get all

--- src/modules/sharenote/queries/sharenotes.handler.ts
+++ src/modules/sharenote/queries/sharenotes.handler.ts
@@ -1,13 +1,19 @@
+import { SharenoteEntityRepository } from './../db/sharenote-entity.repository';
 import { SharenoteDto } from '../dtos/sharenote.dto';
 import { SharenoteDtoRepository } from './../db/sharenote-dto.repository';
 import { SharenotesQuery } from './sharenotes.query';
 import { QueryHandler, IQueryHandler } from '@nestjs/cqrs';
 import { SharenotesDto } from '../dtos/sharenotes.dto';
+import { Sharenote } from '../Sharenote';
 
 @QueryHandler(SharenotesQuery)
 export class SharenotesQueryHandler implements IQueryHandler<SharenotesQuery> {
-  constructor(private readonly sharenoteDtoRepository: SharenoteDtoRepository) {}
-  async execute(): Promise<SharenotesDto[]> {
-    return this.sharenoteDtoRepository.findAll();
+  constructor(
+    private readonly sharenoteDtoRepository: SharenoteDtoRepository,
+    private readonly sharenoteRepository: SharenoteEntityRepository,
+  ) {}
+
+  async execute(): Promise<Sharenote[]> {
+    return this.sharenoteRepository.findAll();
   }
 }
