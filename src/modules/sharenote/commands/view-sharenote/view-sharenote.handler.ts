1b57e8926a0dcaad209e4306d2e0bc4a8706b4fe
Chawapon <chawapon2001@gmail.com>
2022-11-14 17:30:30 +0700

    fix api  Like View

--- src/modules/sharenote/commands/view-sharenote/view-sharenote.handler.ts
+++ src/modules/sharenote/commands/view-sharenote/view-sharenote.handler.ts
@@ -1,18 +1,25 @@
 import { SharenoteEntityRepository } from '../../db/sharenote-entity.repository';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { ViewSharenoteCommand } from './view-sharenote.command';
+import { Sharenote } from '../../Sharenote';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 @CommandHandler(ViewSharenoteCommand)
 export class ViewSharenoteCommandHandler implements ICommandHandler {
   constructor(private readonly sharenoteEntityRepository: SharenoteEntityRepository) {}
 
   async execute({ userId, likeSharenoteDto }: ViewSharenoteCommand): Promise<any> {
-    const sharenote = await this.sharenoteEntityRepository.findOneById(likeSharenoteDto.sharenoteId);
+    let sharenote: Sharenote;
+    try {
+      sharenote = await this.sharenoteEntityRepository.findOneById(likeSharenoteDto.sharenoteId);
+    } catch (e) {
+      return new HttpException('NOT FOUND Sharenote', HttpStatus.NOT_FOUND);
+    }
 
     sharenote.addviewCount();
 
     await this.sharenoteEntityRepository.findOneAndReplaceById(sharenote.getId(), sharenote);
 
-    return;
+    return sharenote;
   }
 }
